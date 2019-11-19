c325741e42f4b05e5d78c1c84a0598db6457d19a
adeltancsik <tancsikadel@gmail.com>
2019-11-19 22:02:15 +0100

    Add styling to AddAdForm

--- src/components/AddAdForm.js
+++ src/components/AddAdForm.js
@@ -1,60 +1,83 @@
 import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 export default function AddAdForm(props) {
   return (
     <div className="adform">
       <h3>Add a new advertisement:</h3>
-      <form onSubmit={props.onSubmit}>
-        <label>Title:</label>
-        <input
-          type="text"
-          name="title"
-          value={props.values.title}
-          onChange={props.onChange}
-          placeholder="title"
-        />
-        <label>Description:</label>
-        <input
-          type="text"
-          name="description"
-          value={props.values.description}
-          onChange={props.onChange}
-          placeholder="description"
-        />
-        <label>URL:</label>
-        <input
-          type="text"
-          name="url"
-          value={props.values.url}
-          onChange={props.onChange}
-          placeholder="url"
-        />
-        <label>Price:</label>
-        <input
-          type="text"
-          name="price"
-          value={props.values.price}
-          onChange={props.onChange}
-          placeholder="price"
-        />
-        <label>E-mail:</label>
-        <input
-          type="text"
-          name="email"
-          value={props.values.email}
-          onChange={props.onChange}
-          placeholder="email"
-        />
-        <label>Phone number:</label>
-        <input
-          type="text"
-          name="phone"
-          value={props.values.phone}
-          onChange={props.onChange}
-          placeholder="phone"
-        />
-        <input type="submit" value="Submit" className="submit" />
-      </form>
+      <Form onSubmit={props.onSubmit}>
+        <Row>
+          <Form.Group>
+            <Col>
+              <Form.Control
+                type="text"
+                name="title"
+                value={props.values.title}
+                onChange={props.onChange}
+                placeholder="Title"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group>
+            <Col>
+              <Form.Control
+                type="text"
+                name="description"
+                value={props.values.description}
+                onChange={props.onChange}
+                placeholder="Description"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group>
+            <Col>
+              <Form.Control
+                type="text"
+                name="url"
+                value={props.values.url}
+                onChange={props.onChange}
+                placeholder="URL"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group>
+            <Col>
+              <Form.Control
+                type="text"
+                name="price"
+                value={props.values.price}
+                onChange={props.onChange}
+                placeholder="Price in €"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group>
+            <Col>
+              <Form.Control
+                type="text"
+                name="email"
+                value={props.values.email}
+                onChange={props.onChange}
+                placeholder="E-mail"
+              />
+            </Col>
+          </Form.Group>
+          <Form.Group>
+            <Col>
+              <Form.Control
+                type="text"
+                name="phone"
+                value={props.values.phone}
+                onChange={props.onChange}
+                placeholder="Phone number"
+              />
+            </Col>
+          </Form.Group>
+        </Row>
+        <Button variant="outline-dark" type="submit" value="Submit" size="sm">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
